128bd48cd9e48c4b9065a0268964c984d7f06d58
Christian Walter <cfwalter@users.noreply.github.com>
2017-11-27 10:37:42 -0500

    Update 03.py

--- 03.py
+++ 03.py
@@ -1,28 +1,14 @@
 #!/usr/bin/env python3
-# WIP
 __author__ = "Christian F. Walter"
 import math
 
-def is_prime(n, primes):
-  s = math.sqrt(n)
-  for p in primes:
-    if n % p == 0:
-      return False
-    if p > s:
-      return True
-  return True
+def primeFactors(n):
+    i = 5
+    sn = math.sqrt(n)
+    while i < sn:
+        while n % i== 0:
+            print i
+        i += 2
 
 if __name__ == "__main__":
-  primes = []
-  p_factors = []
-  for n in range(2,600851475143):
-    if n % 100000 == 0:
-      print(n)
-    if is_prime(n, primes):
-      primes.append(n)
-      if 600851475143 % n == 0:
-        p_factors.append(n)
-
-  print('*' * 8)
-  print(p_factors[-1])
-
+  primeFactors(600851475143)

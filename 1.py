c24bbde5163bda636206342aa6624fb9fa6890a1
Christian Walter <christian.walter.ford@gmail.com>
2017-11-27 08:47:10 -0500

    Rename files

--- 1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-__author__ = "Christian F. Walter"
-
-if __name__ == "__main__":
-  three = range(0, 1000, 3)
-  five = range(0, 1000, 5)
-  fifteen = range(0, 1000, 15)
-  print(sum(three) + sum(five) - sum(fifteen))
-

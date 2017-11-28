209cc168d8bf4ec8ffecb05c3eee7a10e77a08d3
Christian Walter <christian.walter.ford@gmail.com>
2017-11-27 23:32:12 -0500

    28.py

---
+++ 28.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+__author__ = "Christian F. Walter"
+
+from math import factorial
+
+if __name__ == "__main__":
+  # from bottom to top, sum max of two numbers below
+  total = 1
+  n = 1
+  skip = 2
+  cap = 1001**2
+  while n < cap:
+    total += (n + skip) + (n + skip*2) + (n + skip*3) + (n + skip*4)
+    n = (n + skip*4)
+    skip += 2
+
+  print(total)

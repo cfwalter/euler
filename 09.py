d7b3c2e0ce4ba760fb27e009e31eee15f8e02a3e
Christian Walter <cfwalter@users.noreply.github.com>
2017-11-28 10:37:57 -0500

    9

--- 09.py
+++ 09.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python3
-# WIP
 __author__ = "Christian F. Walter"
-import math
-from functools import reduce
-from operator import mul
+
+def divisors(n):
+  d = []
+  for i in range(1, int(n/2+1)):
+    if int(n) % i == 0: d.append(i)
+  d.append(n)
+  return d
 
 if __name__ == "__main__":
-  for i in range(30):
-    for j in range(i):
-      if (i**2 + j**2
+  # use Dickenson's method, check if sum=1000
+  # https://en.wikipedia.org/wiki/Formulas_for_generating_Pythagorean_triples#Dickson.27s_method
+  for r in range(2,200,2):
+    div = divisors(r**2 / 2)
+    for d in div:
+      s = d
+      t = (r**2 / 2) / d
+      if (r+s) + (r+t) + (r+s+t) == 1000:
+        print((r+s), (r+t), (r+s+t))
+        print((r+s) * (r+t) * (r+s+t))

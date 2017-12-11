ba265b3df1b46c16b3071e612d052621f330e2b2
Christian Walter <cfwalter@users.noreply.github.com>
2017-12-11 18:09:13 -0500

    Update 24.py

--- 24.py
+++ 24.py
@@ -3,6 +3,34 @@
 __author__ = "Christian F. Walter"
 from math import factorial
 
+def get_perm_fact_list(num, digits):
+  s = str(digits)
+  fact_list = []
+  fact_sum = 0
+  mod = False
+  for i in range(len(s)-1,-1,-1):
+    if mod == True:
+      p = -1
+    else:
+      p = (num - fact_sum) // factorial(i)
+      if (num - fact_sum) % factorial(i) == 0:
+        mod = True
+        p -= 1
+    fact_list.append(p)
+    fact_sum += p * factorial(i)
+  return fact_list
+
+def get_perm(digits, fact_list):
+  s = sorted(str(digits))
+  p = ''
+  for f in fact_list:
+    p += s.pop(f)
+  return p
+
 if __name__ == "__main__":
-  perms = [int(''.join(p)) for p in permutations('0123456789')]
-  perms[999999]
+  # perms = [int(''.join(p)) for p in permutations('0123456789')]
+  # perms[999999]
+  n = 1000000
+  digits = 1234567890
+  l = get_perm_fact_list(n, digits)
+  print(get_perm(digits, l))

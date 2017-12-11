24ed8a4cf845693b2962b9391879de5df692d099
Christian Walter <cfwalter@users.noreply.github.com>
2017-12-11 17:45:40 -0500

    24

--- 24.py
+++ 24.py
@@ -1,2 +1,8 @@
-# 2*9! + 6*8! + 6*7! + 2*6! + 5*5! + 4! + 2*3! + 2*2!
-# WIP
+#!/usr/bin/env python3
+# 0*10! + 2*9! + 6*8! + 6*7! + 2*6! + 5*5! + 1*4! + 2*3! + 2*2! + 0*1!
+__author__ = "Christian F. Walter"
+from math import factorial
+
+if __name__ == "__main__":
+  perms = [int(''.join(p)) for p in permutations('0123456789')]
+  perms[999999]

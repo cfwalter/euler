a103d280f34cb047d0c30c0ea838ebca5f4ddbf5
Christian Walter <cfwalter@users.noreply.github.com>
2020-08-31 18:21:55 -0400

    Update 070.py

--- 070.py
+++ 070.py
@@ -1,45 +1,34 @@
-def is_permutation(a, b):
-    return sorted(str(a)) == sorted(str(b))
+from math import log10
 
-def gen_primes(n):
-    sieve = [True] * n
-    for i in range(3,int(n**0.5)+1,2):
-        if sieve[i]:
-            sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
-    return [2] + [i for i in range(3,n,2) if sieve[i]]
+def phi_1_to_n(n):
+    phi = [i for i in range(n)]
+    for i in range(2,n):
+        if phi[i] == i:
+            for j in range(i, n, i):
+                phi[j] -= phi[j] // i
+    return phi
 
-def gen_compounds(n):
-    sieve = [True] * n
-    for i in range(3,int(n**0.5)+1,2):
-        if sieve[i]:
-            sieve[i*i::2*i]=[False]*((n-i*i-1)//(2*i)+1)
-    return [1] + [i for i in range(3, n) if not sieve[i] or i%2==0]
+def is_permutation(x, y):
+    if int(log10(x)) != int(log10(y)):
+        return False
+    dig_count = [0 for i in range(10)]
+    while x:
+        dig_count[x%10] += 1
+        x = x//10;
+    while y:
+        dig_count[y%10] -= 1
+        y = y//10;
+    return not any(dig_count)
 
-def totient(n, pl):
-    sqn = n**0.5
-    num = n
-    den = 1
-    for p in pl:
-        if p > sqn:
-            break
-        if n % p == 0:
-            num *= p-1
-            den *= p
-    return num // den
 
 n = 10**7
-pl = gen_primes(n)
-cl = gen_compounds(n)
-cl.remove(1)
-pal_list = []
-for i in cl:
-    t = totient(i, pl)
-    if is_permutation(i, t):
-        pal_list.append((i, t))
+phi = phi_1_to_n(n)
+phi_ratio = [(i, i/phi[i]) for i in range(2,n) if is_permutation(i, phi[i])]
+min_ratio = n
+min_index = 0
+for i, r in phi_ratio:
+    if r < min_ratio:
+        min_ratio = r
+        min_index = i
 
-min_ratio = 100
-min_n = 0
-for n, t in pal_listtoti:
-    if n/t < min_ratio:
-        min_ratio = n/t
-        min_n = n
+print(f'{min_index}: {min_ratio}')
